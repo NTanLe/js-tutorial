1845fa52e2cd80d1d6e0297597366327d8c2ee0b
NhatTanLe <tan0898543121@gmail.com>
2024-08-26 12:35:33 +0700

    Make the item quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -68,20 +68,20 @@ document.querySelectorAll('.js-add-to-cart')
                     matchingItem = item;
                 }
             });
-
+            const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+            const quantity = Number(quantitySelector.value);
             if (matchingItem) {
-                matchingItem.quantity += 1;
+                matchingItem.quantity += quantity;
             } else {
                 cart.push({
                     productId: productId,
-                    quantity: 1
+                    quantity: quantity
                 });
             }
             console.log(cart)
             let cartQuantity = 0;
-
             cart.forEach((item) => {
-                cartQuantity += item.quantity;
+                cartQuantity += 1;
             });
 
             document.querySelector('.js-cart-quantity')
